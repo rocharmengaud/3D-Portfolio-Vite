1d3f23b9ad4ab033ce09e90164a22b13cdb7f125
Roch Armengaud <armengaud.roch@live.fr>
2023-08-03 17:04:47 +0200

    fixed fallback for 3D model; did some cleaning

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,44 +1,30 @@
-import { Suspense, useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const screens = useGLTF('./screens/scene.gltf')
+  const screens = useGLTF('./screens/scene.gltf');
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.5} groundColor='black' />
+      <hemisphereLight intensity={1.5} groundColor="black" />
       <pointLight intensity={6} />
-      <primitive 
-      object={screens.scene}
-      scale={0.8}
-      position={[0, -3.5, 0]}
-      rotation={[0, 1.3, 0]}
-      />
+      <primitive object={screens.scene} scale={0.8} position={[0, -3.5, 0]} rotation={[0, 1.3, -0.03]} />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
   return (
-    <Canvas 
-    frameloop='demand'
-    shadows
-    camera={{position: [20, 3, 5], fov: 30}}
-    gl={{ preserveDrawingBuffer: true}}
-    >
-      <Suspense /*fallback={<CanvasLoader />}*/>
-        <OrbitControls 
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        />
+    <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 30 }} gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas
+export default ComputersCanvas;
