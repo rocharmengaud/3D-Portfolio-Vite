6a7d6fd9bc1dc5720a76f94348789a45eb25657a
Roch Armengaud <armengaud.roch@live.fr>
2023-08-04 18:50:22 +0200

    Playing w layout and animations

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,19 +2,24 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
 
-// This Higher Order Component is used to animate things on screen
+// This Higher Order Component is used as a layout for all the important components AND to auto-scroll when clicking things
 // We get the idName here from export defaults at the end of files. example : export default SectionWrapper(About, 'about')
 const SectionWrapper = (Component, idName) =>
   function HOC() {
     return (
       <motion.section
+        // this variants animates the section
         variants={staggerContainer()}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: true, amount: 0.25 }}
+        // false, to have animations repeat when you scroll back to them
+        // amount is the duration in seconds
+        viewport={{ once: false, amount: 0.25 }}
+        // this classname lays out each section with some padding, centering and limiting width
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         {/* the hash-span is a custom classname in index.css */}
+        {/* this span makes the auto-scroll work */}
         <span className="hash-span" id={idName}>
           &nbsp;
         </span>
