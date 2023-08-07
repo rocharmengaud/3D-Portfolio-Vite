7c762dedfe894a9027cab6f4acc5a1b07fc1c7c9
Roch Armengaud <armengaud.roch@live.fr>
2023-08-07 15:56:34 +0200

    Work exp section done, placeholders atm

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,6 +31,7 @@ const ServiceCard = ({ title, icon, index }) => {
 const About = () => {
   return (
     <>
+      {/* textVariant() animates text */}
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
